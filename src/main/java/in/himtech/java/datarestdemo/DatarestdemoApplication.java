de89e1218d8b9177f7b7c618fc720596abe4af14
hupadhyay <himanshu2703@gmail.com>
2019-07-15 21:56:18 -0500

    comment added

--- src/main/java/in/himtech/java/datarestdemo/DatarestdemoApplication.java
+++ src/main/java/in/himtech/java/datarestdemo/DatarestdemoApplication.java
@@ -8,6 +8,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
+/**
+ * This is a demo Spring Data Rest Project 
+ */
 @SpringBootApplication
 public class DatarestdemoApplication {
 
